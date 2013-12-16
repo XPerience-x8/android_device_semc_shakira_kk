1166b746a38d7ca9372591b0a124421881b0b92e
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2013-12-16 04:00:16 -0600

    Fix derp with kernel build x_x

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,14 +1,3 @@
--include device/semc/msm7x27-common/BoardConfigCommon.mk
--include vendor/semc/shakira/BoardConfigVendor.mk
-
-TARGET_BOOTLOADER_BOARD_NAME := shakira
-TARGET_OTA_ASSERT_DEVICE := E15i,E15a,E16i,E16a,shakira
-PRODUCT_BUILD_PROP_OVERRIDES += TARGET_BOOTLOADER_BOARD_NAME=shakira
-BOARD_CAMERA_NO_AUTOFOCUS := true
-BOARD_LIBSENSORS_AKM8975 := true
-
-# Kernel
-#TARGET_KERNEL_CONFIG := cyanogenmod_shakira_defconfig
 
 #
 # Copyright (C) 2012 The Android Open-Source Project
@@ -25,6 +14,19 @@ BOARD_LIBSENSORS_AKM8975 := true
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+-include device/semc/msm7x27-common/BoardConfigCommon.mk
+-include vendor/semc/shakira/BoardConfigVendor.mk
+
+TARGET_BOOTLOADER_BOARD_NAME := shakira
+TARGET_OTA_ASSERT_DEVICE := E15i,E15a,E16i,E16a,shakira
+PRODUCT_BUILD_PROP_OVERRIDES += TARGET_BOOTLOADER_BOARD_NAME=shakira
+BOARD_CAMERA_NO_AUTOFOCUS := true
+BOARD_LIBSENSORS_AKM8975 := true
+
+# Kernel
+TARGET_KERNEL_CONFIG := cyanogenmod_shakira_defconfig
+
 
 # Target
 TARGET_ARCH            := arm
